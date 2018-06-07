7e50cb397c791fda0b9d2e0ab7608a8b98763c71
Nicolas Schejtman <nschejtman93@gmail.com>
2018-06-07 12:41:19 -0300

    Setup aws ec2 g3.4xlarge with Tensforflow GPU v0.3

--- ubuntu/bash/aws-setup-g3.4xlarge-tensorflow-gpu.sh
+++ ubuntu/bash/aws-setup-g3.4xlarge-tensorflow-gpu.sh
@@ -3,6 +3,10 @@
 # Ubuntu 16.04 LTS      OK                                                     #
 ################################################################################
 
+# Create a handling directory for the process
+mkdir ~/TensorFlow_GPU_Setup_Dir
+cd ~/TensorFlow_GPU_Setup_Dir
+
 # First check that some core packages are installed
 sudo apt-get -y update
 sudo apt-get -y install libglu1-mesa libxi-dev libxmu-dev -y
@@ -18,28 +22,33 @@ sudo ./NVIDIA-Linux-x86_64-367.44.run -silent
 # Check the driver was intstalled correclty
 nvidia-smi
 
-# Install CUDA 8
-wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
-sudo chmod +x cuda_8.0.61_375.26_linux-run
-sudo ./cuda_8.0.61_375.26_linux-run --driver -silent
-sudo ./cuda_8.0.61_375.26_linux-run --toolkit -silent
-sudo ./cuda_8.0.61_375.26_linux-run --samples –silent
+# Install CUDA 8 TODO
+# wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+# sudo chmod +x cuda_8.0.61_375.26_linux-run
+# sudo ./cuda_8.0.61_375.26_linux-run --driver -silent
+# sudo ./cuda_8.0.61_375.26_linux-run --toolkit -silent
+# sudo ./cuda_8.0.61_375.26_linux-run --samples –silent
 
-echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"' >> ~/.bashrc
-echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
-source ~/.bashrc
+# echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"' >> ~/.bashrc
+# echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+# source ~/.bashrc
+
+# Install CUDA 9
+sudo apt-get -y install cuda-9-0
+sudo cuda-9-0 --driver -silent
+sudo cuda-9-0 --toolkit -silent
+sudo cuda-9-0 --samples –silent
 
 # Install CUDNN V5.1+
-mkdir ~/Downloads
-cd ~/Downloads/
 wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/cudnn-8.0-linux-x64-v6.0.tgz
-sudo tar –xvzf cudnn-8.0-linux-x64-v6.0.tgz
-sudo cp -P cuda/lib64 /usr/local/cuda
-sudo cp -P cuda/include/cudnn.h /usr/local/cuda/include/
+tar –xvzf cudnn-8.0-linux-x64-v6.0.tgz
+sudo cp -r -P cuda/lib64 /usr/local/cuda
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include/
 
 # Install LIBCUPTI-DEV library for CUDA profiling
-sudo apt-get install libcupti-dev
+sudo apt-get -y install libcupti-dev
 
 # Install Tensorflow
+sudo apt-get -y install python3-pip
 pip3 install tensorflow
 pip3 install tensorflow-gpu
