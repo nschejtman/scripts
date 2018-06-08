fd546e197b7e2da12ba0a2d91a41c251285562cf
Nicolas Schejtman <nschejtman93@gmail.com>
2018-06-08 10:53:21 -0300

    Added new dependencies to install-docker.sh

--- ubuntu/bash/install-docker.sh
+++ ubuntu/bash/install-docker.sh
@@ -4,7 +4,13 @@ sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common \
+    libltdl7 \
+    aufs-tools \
+    cgroupfs-mount \
+    cgroup-lite \
+    pigz \
+    mountall
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
