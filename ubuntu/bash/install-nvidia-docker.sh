f93564c7fea0b44f1084f521982605baf7593772
Nicolas Schejtman <nschejtman93@gmail.com>
2018-06-08 12:55:46 -0300

    Deleted untested files

--- ubuntu/bash/install-nvidia-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Install nvidia-docker and nvidia-docker-plugin
-sudo apt-add-repository multiverse
-sudo apt update
-sudo apt install nvidia-modprobe
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
-sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
